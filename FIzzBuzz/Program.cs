f2f7ff118aba2af0d067993b954d4279ad3017f5
DHILL2123 <114453911+DHILL2123@users.noreply.github.com>
2022-12-29 14:51:39 -0600

    Add files via upload

---
+++ FIzzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace FizzBuzz
+{
+    public class FizzBuzzTest
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz(20);
+        }
+
+
+        public static void FizzBuzz(int num)
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                string result = "";
+
+                if (i % 3 == 0)
+                    result += "Fizz";
+
+                if (i % 5 == 0)
+                    result += "Buzz";
+
+                if (result == "")
+                    result = i.ToString();
+
+
+                Console.WriteLine($"{result}");
+
+            }
+
+        }
+
+
+    }   }
+    
